a39caf6d824d504dbedfc132ddcfc1f2262f3db5
Alfonso <alfonso.garcia2324@gmail.com>
2019-10-22 09:03:44 -0700

    Reorganized things and added css normalize

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Login = () => {
+    const [token, setToken] = useState("");
+
+    return (
+        <div>
+            <p>test</p>
+        </div>
+    );
+};
+
+export default Login;
