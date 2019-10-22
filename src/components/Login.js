ccab06cb487c092a2f4e15332cb4ccf8d520ef6b
Alfonso <alfonso.garcia2324@gmail.com>
2019-10-22 09:10:33 -0700

    periodic commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
     const [token, setToken] = useState("");
 
     return (
-        <div>
+        <div className="Login">
             <p>test</p>
         </div>
     );
