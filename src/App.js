e76b4d5b2be0134855121fe929b99e8cebab3c98
Alfonso <alfonso.garcia2324@gmail.com>
2019-10-22 10:21:33 -0700

    implemented the axios get request for init

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// Styles import
 import "./styles/App.scss";
 
 // Component imports
 import Login from "./components/Login";
 
-function App() {
+const App = () => {
+    const [backendURL] = useState("https://lambda-treasure-hunt.herokuapp.com");
+
+    useEffect(() => {
+        localStorage.clear();
+    }, []);
+
     return (
         <div className="App">
-            <Login />
+            <Login backendURL={backendURL} />
         </div>
     );
-}
+};
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,61 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const Login = () => {
-    const [token, setToken] = useState("");
+const Login = props => {
+    // Props destructuring -----------------
+    const { backendURL } = props;
 
+    // State hooks -----------------
+    const [token, setToken] = useState("");
+    const [error, setError] = useState({});
+    const [roomInfo, setRoomInfo] = useState({});
+
+    // Handler functions -----------------
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios
+            .get(`${backendURL}/api/adv/init`, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Token ${token}`
+                }
+            })
+            .then(res => {
+                setError({});
+                setRoomInfo(res.data);
+                console.log(res.data);
+            })
+            .catch(err => {
+                setError({
+                    Error: err.response.data.detail,
+                    status: err.response.status
+                });
+                setRoomInfo({});
+                console.log(err);
+            });
+
+        localStorage.setItem("token", token);
+    };
+
+    const handleChange = e => {
+        setToken(e.target.value);
+    };
+
+    const reset = e => {
+        setToken("");
+    };
+
+    // Return -----------------
     return (
         <div className="Login">
-            <p>test</p>
+            <form onSubmit={handleSubmit}>
+                <label>Enter your API key:</label>
+                <input type="password" value={token} onChange={handleChange} />
+                <button type="submit">Submit</button>
+                <button type="reset" onClick={reset}>
+                    Clear
+                </button>
+            </form>
         </div>
     );
 };
